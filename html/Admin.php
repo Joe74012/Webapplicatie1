951304503af397069a2eab5a36c8a41d8b2414d8
Joe74012 <1210764@student.roc-nijmegen.nl>
2025-04-14 21:15:52 +0200

    Heb logo's veranderd naar tekst om het duidelijker te maken.

--- html/Admin.php
+++ html/Admin.php
@@ -10,13 +10,15 @@ if (!isset($_SESSION["admin"]) || $_SESSION["admin"] != true) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin | Toko Rinus</title>
     <link rel="stylesheet" href="CSS/Style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <script src="https://kit.fontawesome.com/61f819b69c.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Lexend:wght@100..900&family=Lilita+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rowdies:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Lexend:wght@100..900&family=Lilita+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rowdies:wght@300;400;700&display=swap"
+          rel="stylesheet">
 </head>
 <body>
 <header>
@@ -43,9 +45,9 @@ if (!isset($_SESSION["admin"]) || $_SESSION["admin"] != true) {
         </div>
     </nav>
 </header>
-    <div class="Admin-Uitleg-Rij">
-        <h2 class="Zwarte-Text">Welkom bij de admin pagina wat wilt u doen?</h2>
-    </div>
+<div class="Admin-Uitleg-Rij">
+    <h2 class="Zwarte-Text">Welkom bij de admin pagina wat wilt u doen?</h2>
+</div>
 <div class="Admin-Navigatie-Rij">
     <a href="Toevoegen.php">
         <div class="Groene-Knop">
@@ -65,12 +67,12 @@ while ($Menu = $stmt->fetch()) {
     <div class='Bewerk-Verwijder-Rij'>
         <a href='Delete.php?id=" . $Menu["Id"] . "'>
         <div class='Rode-Knop'>
-        <i class='fa-solid fa-trash'></i>
+            <h2 class='Witte-Text'>Verwijder</h2>
         </div>
         </a>
         <a href='Bewerk.php?id=" . $Menu["Id"] . "'>
         <div class='Blauwe-Knop'>
-        <i class='fa-solid fa-pen'></i>
+            <h2 class='Witte-Text'>Bewerk</h2>
         </div>
         </a>
     </div>
